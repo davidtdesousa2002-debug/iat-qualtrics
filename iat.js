02db686fe8ed383b35ca71dacea44adc76047a77
davidtdesousa2002-debug <davidt.desousa2002@gmail.com>
2026-03-17 15:46:16 -0300

    Refactor IAT to use new categories and stimuli.js

--- iat.js
+++ iat.js
@@ -1,98 +1,57 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension){
- var API = new APIConstructor();
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/qiat6.js'], function(APIConstructor, iatExtension){
 
-	return iatExtension({
-		practiceCategory1 : 
-			{
-				name : 'Coração',
-				title : {
-					media : {word : 'coração'},
-					css : {color:'#31b404','font-size':'1.8em'},
-					height : 4,
-					startStimulus : { 
-						media : {word : 'Cachorros, Cavalos, Vacas, Leões'}, 
-						css : {color:'#31b404','font-size':'1em'}, 
-						height : 2
-					}
-				}, 
-				stimulusMedia : [
-					{word : 'Cachorros'}, 
-					{word : 'Cavalos'}, 
-					{word : 'Leões'}, 
-					{word : 'Vacas'}
-				], 
-				stimulusCss : {color:'#31b404','font-size':'2em'}
-			},	
-			practiceCategory2 : 
-			{
-				name : 'Aves', 
-				title : {
-					media : {word : 'Aves'}, 
-					css : {color:'#31b404','font-size':'1.8em'}, 
-					height : 4,
-					startStimulus : {
-						media : {word : 'Pombos, Cisnes, Corvos, Gralhas'}, 
-						css : {color:'#31b404','font-size':'1em'}, 
-						height : 2
-					}
-				}, 
-				stimulusMedia : [
-					{word : 'Pombos'}, 
-					{word : 'Cisnes'}, 
-					{word : 'Corvos'}, 
-					{word : 'Gralhas'}
-				], 
-				stimulusCss : {color:'#31b404','font-size':'2em'}
-			},
-			categories : [
-				{
-					name : 'Pessoas Negras',
-					title : {
-						media : {word : 'Pessoas Negras'},
-						css : {color:'#31b404','font-size':'1.8em'},
-						height : 4,
-						startStimulus : { 
-							media : {image : 'blacks.jpg'}, 
-							css : {color:'#31b404','font-size':'1em'}, 
-							height : 13
-						}
-					}, 
-					stimulusMedia : [
-						{image : 'black1.jpg'}, 
-						{image : 'black2.jpg'}, 
-						{image : 'black3.jpg'}, 
-						{image : 'black4.jpg'}, 
-						{image : 'black5.jpg'}, 
-						{image : 'black6.jpg'}
-					], 
-					stimulusCss : {color:'#31b404','font-size':'2em'}
-				},	
-				{
-					name : 'Pessoas Brancas', 
-					title : {
-						media : {word : 'Pessoas Brancas'}, 
-						css : {color:'#31b404','font-size':'1.8em'}, 
-						height : 4,
-						startStimulus : {
-							media : {image : 'whites.jpg'},  
-							css : {color:'#31b404','font-size':'1em'}, 
-							height : 13
-						}
-					}, 
-					stimulusMedia : [
-						{image : 'yf1.jpg'}, 
-						{image : 'yf4.jpg'}, 
-						{image : 'yf5.jpg'}, 
-						{image : 'ym2.jpg'}, 
-						{image : 'ym3.jpg'}, 
-						{image : 'ym5.jpg'}
-					], 
-					stimulusCss : {color:'#31b404','font-size':'2em'}
-				}
-			],
-			base_url : {
-				image : 'https://baranan.github.io/minno-tasks/images/'
-			},
-			taskName: 'BIAT_Racismo_PT'
-	});
+var API = new APIConstructor();
+
+return iatExtension({
+
+    practiceCategory1 : {
+        name : 'Pessoas Negras',
+        title : {media : {word : 'Pessoas Negras'}, css : {color:'#31b404','font-size':'1.8em'}, height : 4},
+        stimulusMedia : [{word : 'Exemplo 1'},{word : 'Exemplo 2'}]
+    },
+
+    practiceCategory2 : {
+        name : 'Pessoas Brancas',
+        title : {media : {word : 'Pessoas Brancas'}, css : {color:'#31b404','font-size':'1.8em'}, height : 4},
+        stimulusMedia : [{word : 'Exemplo 3'},{word : 'Exemplo 4'}]
+    },
+
+    category1 : {
+        name : 'Pessoas Negras',
+        title : {media : {word : 'Pessoas Negras'}, css : {color:'#31b404','font-size':'1.8em'}, height : 4},
+        stimulusMedia : [{word : 'Exemplo 1'},{word : 'Exemplo 2'}]
+    },
+
+    category2 : {
+        name : 'Pessoas Brancas',
+        title : {media : {word : 'Pessoas Brancas'}, css : {color:'#31b404','font-size':'1.8em'}, height : 4},
+        stimulusMedia : [{word : 'Exemplo 3'},{word : 'Exemplo 4'}]
+    },
+
+    attribute1 : {
+        name : 'Agradável',
+        title : {media : {word : 'Agradável'}, css : {color:'#0000FF','font-size':'1.8em'}, height : 4},
+        stimulusMedia : [
+            {word : 'Alegria'},
+            {word : 'Amor'},
+            {word : 'Feliz'},
+            {word : 'Prazer'},
+            {word : 'Bom'}
+        ]
+    },
+
+    attribute2 : {
+        name : 'Desagradável',
+        title : {media : {word : 'Desagradável'}, css : {color:'#0000FF','font-size':'1.8em'}, height : 4},
+        stimulusMedia : [
+            {word : 'Ódio'},
+            {word : 'Dor'},
+            {word : 'Triste'},
+            {word : 'Ruim'},
+            {word : 'Terrível'}
+        ]
+    },
+
+    base_url : {image : ''}
+});
 });
