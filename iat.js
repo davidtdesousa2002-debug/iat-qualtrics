0073f6b746118b8671034a6429bad6c54e8d602b
davidtdesousa2002-debug <davidt.desousa2002@gmail.com>
2026-03-17 15:23:50 -0300

    Refactor categories and attributes in iat.js

--- iat.js
+++ iat.js
@@ -2,134 +2,97 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
  var API = new APIConstructor();
 
 	return iatExtension({
-
 		practiceCategory1 : 
-		{
-			name : 'Mamíferos',
-			title : {
-				media : {word : 'Mamíferos'},
-				css : {color:'#31b404','font-size':'1.8em'},
-				height : 4,
-				startStimulus : { 
-					media : {word : 'Cachorros, Cavalos, Vacas, Leões'}, 
-					css : {color:'#31b404','font-size':'1em'}, 
-					height : 2
-				}
-			}, 
-			stimulusMedia : [
-				{word : 'Cachorros'}, 
-				{word : 'Cavalos'}, 
-				{word : 'Leões'}, 
-				{word : 'Vacas'}
-			], 
-			stimulusCss : {color:'#31b404','font-size':'2em'}
-		},	
-
-		practiceCategory2 : 
-		{
-			name : 'Aves', 
-			title : {
-				media : {word : 'Aves'}, 
-				css : {color:'#31b404','font-size':'1.8em'}, 
-				height : 4,
-				startStimulus : {
-					media : {word : 'Pombos, Cisnes, Corvos, Gralhas'}, 
-					css : {color:'#31b404','font-size':'1em'}, 
-					height : 2
-				}
-			}, 
-			stimulusMedia : [
-				{word : 'Pombos'}, 
-				{word : 'Cisnes'}, 
-				{word : 'Corvos'}, 
-				{word : 'Gralhas'}
-			], 
-			stimulusCss : {color:'#31b404','font-size':'2em'}
-		},
-
-		// 🔥 ATRIBUTOS TRADUZIDOS
-		attribute1 : {
-			name : 'Positivo',
-			title : {
-				media : {word : 'Positivo'},
-				css : {color:'#0000FF','font-size':'1.8em'},
-				height : 4
-			},
-			stimulusMedia : [
-				{word : 'Alegria'},
-				{word : 'Amor'},
-				{word : 'Feliz'},
-				{word : 'Bom'}
-			]
-		},
-
-		attribute2 : {
-			name : 'Negativo',
-			title : {
-				media : {word : 'Negativo'},
-				css : {color:'#FF0000','font-size':'1.8em'},
-				height : 4
-			},
-			stimulusMedia : [
-				{word : 'Dor'},
-				{word : 'Ódio'},
-				{word : 'Triste'},
-				{word : 'Ruim'}
-			]
-		},
-
-		categories : [
 			{
-				name : 'Pessoas Negras',
+				name : 'Mamíferos',
 				title : {
-					media : {word : 'Pessoas Negras'},
+					media : {word : 'Mamíferos'},
 					css : {color:'#31b404','font-size':'1.8em'},
 					height : 4,
 					startStimulus : { 
-						media : {image : 'blacks.jpg'}, 
+						media : {word : 'Cachorros, Cavalos, Vacas, Leões'}, 
 						css : {color:'#31b404','font-size':'1em'}, 
-						height : 13
+						height : 2
 					}
 				}, 
 				stimulusMedia : [
-					{image : 'black1.jpg'}, 
-					{image : 'black2.jpg'}, 
-					{image : 'black3.jpg'}, 
-					{image : 'black4.jpg'}, 
-					{image : 'black5.jpg'}, 
-					{image : 'black6.jpg'}
+					{word : 'Cachorros'}, 
+					{word : 'Cavalos'}, 
+					{word : 'Leões'}, 
+					{word : 'Vacas'}
 				], 
 				stimulusCss : {color:'#31b404','font-size':'2em'}
 			},	
-
+			practiceCategory2 : 
 			{
-				name : 'Pessoas Brancas', 
+				name : 'Aves', 
 				title : {
-					media : {word : 'Pessoas Brancas'}, 
+					media : {word : 'Aves'}, 
 					css : {color:'#31b404','font-size':'1.8em'}, 
 					height : 4,
 					startStimulus : {
-						media : {image : 'whites.jpg'},  
+						media : {word : 'Pombos, Cisnes, Corvos, Gralhas'}, 
 						css : {color:'#31b404','font-size':'1em'}, 
-						height : 13
+						height : 2
 					}
 				}, 
 				stimulusMedia : [
-					{image : 'yf1.jpg'}, 
-					{image : 'yf4.jpg'}, 
-					{image : 'yf5.jpg'}, 
-					{image : 'ym2.jpg'}, 
-					{image : 'ym3.jpg'}, 
-					{image : 'ym5.jpg'}
+					{word : 'Pombos'}, 
+					{word : 'Cisnes'}, 
+					{word : 'Corvos'}, 
+					{word : 'Gralhas'}
 				], 
 				stimulusCss : {color:'#31b404','font-size':'2em'}
-			}
-		],
-
-		base_url : {
-			image : 'https://baranan.github.io/minno-tasks/images/'
-		},
-
-		taskName: 'BIAT_Racismo_PT'
+			},
+			categories : [
+				{
+					name : 'Pessoas Negras',
+					title : {
+						media : {word : 'Pessoas Negras'},
+						css : {color:'#31b404','font-size':'1.8em'},
+						height : 4,
+						startStimulus : { 
+							media : {image : 'blacks.jpg'}, 
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 13
+						}
+					}, 
+					stimulusMedia : [
+						{image : 'black1.jpg'}, 
+						{image : 'black2.jpg'}, 
+						{image : 'black3.jpg'}, 
+						{image : 'black4.jpg'}, 
+						{image : 'black5.jpg'}, 
+						{image : 'black6.jpg'}
+					], 
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				},	
+				{
+					name : 'Pessoas Brancas', 
+					title : {
+						media : {word : 'Pessoas Brancas'}, 
+						css : {color:'#31b404','font-size':'1.8em'}, 
+						height : 4,
+						startStimulus : {
+							media : {image : 'whites.jpg'},  
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 13
+						}
+					}, 
+					stimulusMedia : [
+						{image : 'yf1.jpg'}, 
+						{image : 'yf4.jpg'}, 
+						{image : 'yf5.jpg'}, 
+						{image : 'ym2.jpg'}, 
+						{image : 'ym3.jpg'}, 
+						{image : 'ym5.jpg'}
+					], 
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				}
+			],
+			base_url : {
+				image : 'https://baranan.github.io/minno-tasks/images/'
+			},
+			taskName: 'BIAT_Racismo_PT'
 	});
 });
